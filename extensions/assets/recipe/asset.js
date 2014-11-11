066d3f875cfa3211605509185b210ac385ffd8af
Buddhima <buddhimawijeweera@yahoo.com>
2014-11-11 19:49:34 +0530

    Code clean-up - 1

--- extensions/assets/recipe/asset.js
+++ extensions/assets/recipe/asset.js
@@ -5,28 +5,11 @@ var assetLinks = function (user) {
             {
                 title: 'recipes',
                 url: ''
-            },
+            }/*,
             {
                 title: 'Bookmarks',
                 url: 'bookmarks',
                 path: 'bookmarks.jag'
-            }
+            }*/
         ]};
 };
-
-//asset.server = function(ctx) {
-//    var type = ctx.type;
-//    return {
-//        onUserLoggedIn: function() {},
-//        endpoints: {
-//            apis: [{
-//                url: 'assets',
-//                path: 'assets.jag'
-//            },
-//            {
-//                url: 'use_recipe',
-//                path:'use_recipe.jag'
-//            }]
-//        }
-//    };
-//};
